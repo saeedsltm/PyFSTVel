aba71cebb748c1db7b2796f99e39e08e68857885
saeed <saeed.sltm@gmail.com>
2021-12-04 13:28:10 +0330

    Linux version added.

--- PyFSTPSO.py
+++ PyFSTPSO.py
@@ -1,5 +1,5 @@
 from fstpso import FuzzyPSO
-from numpy import array, append, delete, genfromtxt, loadtxt, mean, savetxt, std
+from numpy import array, append, delete, genfromtxt, loadtxt, mean, std
 from multiprocess import Pool
 from string import ascii_letters as al
 from random import sample
@@ -8,14 +8,15 @@ from shutil import copy
 from glob import glob
 from subprocess import getstatusoutput as gso
 import proplot as plt
-
+from utils.hypoellipseLoc import runHypoellipse
+from shutil import rmtree
 """
 Calculate 1D velocity model using FST-PSO method
 for more ifo look at > https://github.com/aresio/fst-pso.
 
 input files: 
 
-    hypoel.pha, hypoel.sta, hypoel.prm, default.cfg 
+    hypoellipse.pha, hypoellipse.sta, default.cfg 
 
 outputs:
 
@@ -31,8 +32,8 @@ ChangeLogs:
 
 #___________________Read input parameter file
 
-if not os.path.exists("fst-pso.par"):
-    with open("fst-pso.par", "w") as f:
+if not os.path.exists("fstpsoVel.par"):
+    with open("fstpsoVel.par", "w") as f:
         f.write("""#################################################
 #
 # Parameter file for FST-PSO program.
@@ -52,7 +53,7 @@ MP_FLAG  = 1                                     # Multiprocessing mode, 0:Disab
 MEASURE  = 1                                     # Quality measure 1 for RMS and 2 for location accuracy."""
                 )
                 
-fst_pso_par = dict(genfromtxt("fst-pso.par", dtype=str, skip_header=6, delimiter="=", autostrip=True))
+fst_pso_par = dict(genfromtxt("fstpsoVel.par", dtype=str, skip_header=6, delimiter="=", autostrip=True))
 for key in fst_pso_par.copy(): fst_pso_par[key.strip()] = fst_pso_par.pop(key)
 
 for key in list(fst_pso_par.keys()):
@@ -61,8 +62,8 @@ for key in list(fst_pso_par.keys()):
 #___________________Define main class
 class Main():
     def __init__(self, plot_flag):
-        if not plot_flag:
-            with open("best_model.dat", "w"): pass
+        # if not plot_flag:
+        #     with open("best_model.dat", "w"): pass
         self.run_id = 1
         self.vpvs = float(fst_pso_par["VpVS_R"])
         self.vel_min = fst_pso_par["VEL_MIN"]
@@ -76,8 +77,15 @@ class Main():
         self.low_bnd = array([self.vel_min, self.vel_max]).T.tolist()
         self.upr_bnd = array([self.dep_min, self.dep_max]).T.tolist()
         self.search_space = self.low_bnd + self.upr_bnd
+        self.resultDir = "results"
+        self.bestModelOut = os.path.join(self.resultDir, "best_model.dat")
+    
+    def makeResultDir(self):
+        if os.path.exists(self.resultDir):
+            rmtree(self.resultDir)
+        os.mkdir(self.resultDir)
 
-    def write_hypoel_prm(self, x, vpvs, name):
+    def write_hypoellipse_prm(self, x, vpvs, name):
         model = x.reshape((2, x.size//2)).T
         with open("{0}.prm".format(name), "w") as f:
             for l in model:
@@ -85,87 +93,85 @@ class Main():
                 f.write("VELOCITY             {vp:4.2f} {z:5.2f} {vpvs:4.2f}\n".format(vp=vp, z=z, vpvs=vpvs))
 
     def write_fst_pso_result(self, result):
-        with open("best_model.dat", "a") as f:
+        with open(self.bestModelOut, "a") as f:
             f.write(" ".join("{0:7.2f}".format(e) for e in result[0].X))
             f.write("\n")
 
     def write_best_model(self, best_model, best_std):
-        with open("best_model.dat", "a") as f:
+        with open(self.bestModelOut, "a") as f:
             f.write("Best Model (Average of all models and StdDev):\n")
             f.write(" ".join("{0:7.2f}".format(e) for e in best_model))
             f.write("\n")
             f.write(" ".join("{0:7.2f}".format(e) for e in best_std))
             f.write("\n")
             
-    def hypoel_obj_f(self, new_model):
-        hypoel_id = "".join(sample(al, 5))
+    def hypoellipse_obj_f(self, new_model):
+        hypoellipse_id = "".join(sample(al, 5))
         vpvs = self.vpvs
-        self.write_hypoel_prm(array(new_model), vpvs, hypoel_id)
-        copy("hypoel.sta", "{0}.sta".format(hypoel_id))
-        copy("hypoel.pha", "{0}.pha".format(hypoel_id))
+        self.write_hypoellipse_prm(array(new_model), vpvs, hypoellipse_id)
+        copy("hypoellipse.sta", "{0}.sta".format(hypoellipse_id))
+        copy("hypoellipse.pha", "{0}.pha".format(hypoellipse_id))
+        runHypoellipse(hypoellipse_id)
         if platform.system() == "Linux":
-            cmd = "hypoell-loc.sh {0} > /dev/null << EOF\nn\nEOF".format(hypoel_id)
-            os.system(cmd)
             # based on rms
             if self.measure == 1:    
-                cmd = "awk '/average rms of all events/' {0}.out".format(hypoel_id)
+                cmd = "awk '/average rms of all events/' {0}.out".format(hypoellipse_id)
                 rms = float(gso(cmd)[1].split()[-1])
-                cmd = "rm {0}*".format(hypoel_id)
+                cmd = "rm {0}*".format(hypoellipse_id)
                 os.system(cmd)                 
                 return rms
             # based on location quality (weighted average in percentage), 100.0 - (a*1.0 + b*0.75 + c*0.50 + d*0.25)
             elif self.measure == 2:
-                cmd = "grep 'percentage' {0}.out | awk '{print ($2*1.0 + $3*0.75 + $4*0.50 + $5*0.25)}'".format(hypoel_id)
+                cmd = "grep 'percentage' {0}.out | awk '{print ($2*1.0 + $3*0.75 + $4*0.50 + $5*0.25)}'".format(hypoellipse_id)
                 quality = float(gso(cmd)[1])
-                cmd = "rm {0}*".format(hypoel_id)
+                cmd = "rm {0}*".format(hypoellipse_id)
                 os.system(cmd)                 
-                return 100.0 - quality            
+                return -quality            
                
         elif platform.system() == "Windows":
-            cmd = "python hypoell-loc.py {0}".format(hypoel_id)
-            os.system(cmd)
             # based on rms
             if self.measure == 1:    
-                cmd = 'findstr /C:"average rms of all events" {0}.out'.format(hypoel_id)
+                cmd = 'findstr /C:"average rms of all events" {0}.out'.format(hypoellipse_id)
                 rms = float(gso(cmd)[1].split()[-1])
-                cmd = "del {0}*".format(hypoel_id)
+                cmd = "del {0}*".format(hypoellipse_id)
                 os.system(cmd)                
                 return rms
             # based on location quality (weighted average in percentage), 100.0 - (a*1.0 + b*0.75 + c*0.50 + d*0.25)
             elif self.measure == 2:
-                cmd = 'findstr /C:"percentage" {0}.out '.format(hypoel_id)
+                cmd = 'findstr /C:"percentage" {0}.out '.format(hypoellipse_id)
                 _, a, b, c, d = gso(cmd)[1].split()
                 quality = float(a)*1.0 + float(b)*0.75 + float(c)*0.50 + float(d)*0.25
-                cmd = "del {0}*".format(hypoel_id)
+                cmd = "del {0}*".format(hypoellipse_id)
                 os.system(cmd)
-                return 100.0 - quality             
+                return -quality             
 
     def parallel_fitness_function(self, particles):
         N = int(fst_pso_par["MP_FLAG"])
         if N == 1: N = os.cpu_count()
         p = Pool(N)
-        all_results = p.map(self.hypoel_obj_f, particles)
+        all_results = p.map(self.hypoellipse_obj_f, particles)
         p.close()
         return all_results
 
     def run_fst_pso(self):
-        FP = FuzzyPSO(logfile="log.dat")
+        FP = FuzzyPSO(logfile=os.path.join(self.resultDir, "log.dat"))
         FP.disable_fuzzyrule_minvelocity()
         FP.set_search_space(self.search_space)
+        best_fitness_out = os.path.join(self.resultDir, "best_fitness_{0:d}.dat".format(self.run_id+1))
         if self.num_md != 0:
             FP.set_swarm_size(self.num_md)
         if fst_pso_par["MP_FLAG"] != 0:
             FP.set_parallel_fitness(self.parallel_fitness_function, skip_test=True)
             if self.num_it != 0:
-                result = FP.solve_with_fstpso(max_iter=self.num_it, dump_best_fitness="best_fitness_{0:d}.dat".format(self.run_id+1))
+                result = FP.solve_with_fstpso(max_iter=self.num_it, dump_best_fitness=best_fitness_out)
             else:
-                result = FP.solve_with_fstpso(dump_best_fitness="best_fitness_{0:d}.dat".format(self.run_id+1))
+                result = FP.solve_with_fstpso(dump_best_fitness=best_fitness_out)
         else:
-            FP.set_fitness(self.hypoel_obj_f, skip_test=True)
+            FP.set_fitness(self.hypoellipse_obj_f, skip_test=True)
             if self.num_it != 0:
-                result = FP.solve_with_fstpso(max_iter=self.num_it, dump_best_fitness="best_fitness_{0:d}.dat".format(self.run_id+1))
+                result = FP.solve_with_fstpso(max_iter=self.num_it, dump_best_fitness=best_fitness_out)
             else:
-                result = FP.solve_with_fstpso(dump_best_fitness="best_fitness_{0:d}.dat".format(self.run_id+1))
+                result = FP.solve_with_fstpso(dump_best_fitness=best_fitness_out)
         self.write_fst_pso_result(result)
 
     def plot_results(self):
@@ -173,15 +179,19 @@ class Main():
             self.ylabel = "RMS (s)"
         elif self.measure == 2:
             self.ylabel = "HQI (weighted average - %)"
-        
+
         with plt.rc.context(abc="a)"):
             fig, axs = plt.subplots(ncols=2, share=0)
         
-        best_fits = [loadtxt(best_fitness) for best_fitness in glob("best_fitness_*")]
-        best_model = mean(loadtxt("best_model.dat"), axis=0)
-        bm_std = std(loadtxt("best_model.dat"), axis=0)
+        best_fits = [loadtxt(best_fitness) for best_fitness in glob(os.path.join(self.resultDir, "best_fitness_*"))]
+        try:
+            best_model = mean(loadtxt(self.bestModelOut), axis=0)
+        except ValueError:
+            print("\n+++ Remove lines 3,4,5 from 'file:results/best_model.dat' and run again.")
+            sys.exit()
+        bm_std = std(loadtxt(self.bestModelOut), axis=0)
         self.write_best_model(best_model, bm_std)
-        self.write_hypoel_prm(best_model, self.vpvs, "hypoel.prm")
+        self.write_hypoellipse_prm(best_model, self.vpvs, os.path.join(self.resultDir, "Final"))
 
         ax = axs[0]
         ax.format(title="Loss")
@@ -225,7 +235,7 @@ class Main():
             ax.fill_between([finvel[c+1], finvel[c+2]], -findep[c+1]-bm_std[j], -findep[c+1]+bm_std[j], color="r", alpha=.2)
             c+=2
         ax.legend(loc=3, ncols=1)
-        fig.save("fst-pso_result.png", bbox_inches="tight")
+        fig.save(os.path.join(self.resultDir, "fst-pso_result.png"), bbox_inches="tight")
         plt.close()
 
 #___________________Run
@@ -233,6 +243,7 @@ if __name__ == "__main__":
     ans = input("\n++++ New run [n] or Only plot results [p]:\n\n")
     if ans.upper() == "N":
         instance = Main(plot_flag=False)
+        instance.makeResultDir()
         for i in range(int(fst_pso_par["NUM_RUN"])):
             instance.run_id = i
             instance.run_fst_pso()
